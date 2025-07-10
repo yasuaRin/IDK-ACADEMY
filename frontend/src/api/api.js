b914278337e22b09ae4ecc481bfd8966b06c4d8b
yasuaRin <dadiaputu@gmail.com>
2025-07-11 05:42:28 +0700

    pkg.json

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,11 +1,8 @@
 import axios from 'axios';
-
-const api = axios.create({ baseURL: `${import.meta.env.VITE_BASE_URL}/api` });
-
+const api = axios.create({ baseURL: 'http://localhost:5000/api' });
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
-
 export default api;
